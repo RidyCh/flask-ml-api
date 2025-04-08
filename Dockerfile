ed6fedc615b53fa36ba8818e305a959ea6a16cb2
Ridy <149645280+RidyCh@users.noreply.github.com>
2025-04-09 00:37:42 +0700

    percobaan terakhir fq

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 # ENV PORT=5000
 ENV FLASK_API_KEY=c43649ac42bc8e0259106ffd7cb9571cda6a03a1010d2c2c6415bab08dbf98e3
 
-EXPOSE $PORT
+EXPOSE 8080
 
 CMD ["sh", "-c", "exec gunicorn --bind 0.0.0.0:$PORT app:app"]
--- app.py
+++ app.py
@@ -65,5 +65,5 @@ def predict():
         return jsonify({'error': str(e), 'trace': str(traceback.format_exc())}), 500
     
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
